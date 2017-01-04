480e0068a193138808f884095a26b4efd1ef576c
Sander van den Akker <sander@sandervandenakker.nl>
2017-01-04 17:11:45 +0100

    feat: Implemented users endpoints.

--- src/resources/companies.js
+++ src/resources/companies.js
@@ -5,31 +5,25 @@ export default class Companies {
   }
 
   /**
-   * Find a company resource.
-   * 
+   * Get a company resource.
+   *
    * @param {string} guid The Global Unique ID of the company.
    * @param {function} cb The callback with two parameters, errror and company.
    * @returns
-   * 
-   * @memberOf Companies
    */
    get(guid, cb) {
      return this.client.get(`companies/${guid}`, {}, cb);
   }
 
   /**
-   * 
-   * Create a company resource.
-   * 
-   * @param {string} guid The Global Unique ID of the company.
-   * @param {object} body The company to create.
+   * Create a new company resource.
+   *
+   * @param {object} body The company document to create.
    * @param {function} cb The callback with two parameters, errror and the newly created company.
    * @returns
-   * 
-   * @memberOf Companies
    */
-  create(guid, body, cb) {
-    return this.client.post(`companies/${guid}`, body, cb);
+  create(body, cb) {
+    return this.client.post('companies', body, cb);
   }
 
   /**
@@ -39,8 +33,6 @@ export default class Companies {
    * @param {object} body The fields to update.
    * @param {function} cb The callback with two parameters, errror and the updated company.
    * @returns
-   *
-   * @memberOf Companies
    */
   update(guid, body, cb) {
     this.client.patch(`companies/${guid}`, body, cb);
@@ -52,8 +44,6 @@ export default class Companies {
    * @param {string} guid The Global Unique ID of the company.
    * @param {function} cb The callback with two parameters, error and a result object with the deleted resource count.
    * @returns
-   *
-   * @memberOf Companies
    */
   delete(guid, cb) {
     return this.client.delete(`companies/${guid}`, cb);
--- src/resources/users.js
+++ src/resources/users.js
@@ -5,16 +5,60 @@ export default class Users {
   }
 
   /**
+   * List users resources.
+   *
+   * @param {object} params Optional parameters, saved for future use.
+   * @param {function} cb The callback with two parameters, errror and the user resource.
+   * @returns
+   */
+  list(params, cb) {
+    return this.client.get('users', params, cb);
+  }
+
+  /**
    * Get a user resource.
    *
    * @param {string} userId The id of the user.
-   * @param {function} cb The callback with two parameters, error and a result object.
+   * @param {object} params Optional parameters, saved for future use.
+   * @param {function} cb The callback with two parameters, errror and the user resource.
    * @returns
-   *
-   * @memberOf Accounts
    */
   get(userId, params, cb) {
     return this.client.get(`users/${guid}`, params, cb);
+  }
+
+  /**
+   * Create a new user resource.
+   *
+   * @param {object} body The company document to create.
+   * @param {function} cb The callback with two parameters, errror and the newly created user.
+   * @returns
+   */
+  create(body, cb) {
+    return this.client.post('users', body, cb);
+  }
+
+  /**
+   * Update a single user resource.
+   *
+   * @param {string} userId The id of the user.
+   * @param {object} body The fields to update.
+   * @param {function} cb The callback with two parameters, errror and the updated user resource.
+   * @returns
+   */
+  update(userId, body, cb) {
+    return this.client.post(`users/${userId}`, body, cb);
+  }
+
+    /**
+   * Delete a single user resource.
+   *
+   * @param {string} userId The id of the user.
+   * @param {function} cb The callback with two parameters, error and a result object with the deleted resource count.
+   * @returns
+   */
+  delete(guid, cb) {
+    return this.client.delete(`companies/${guid}`, cb);
   }
 
 }
